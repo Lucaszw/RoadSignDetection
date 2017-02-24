19afc800ed290008ab9efacfb1d5935f46a3275a
lucaszw <lucas.zeer@gmail.com>
2017-02-23 20:38:48 -0500

    torch input script

--- model/get_caffemodel.sh
+++ model/get_caffemodel.sh
@@ -1,4 +1,4 @@
-URL="https://github.com/magnusja/GTSRB-caffe-model/blob/master/caffe/20151207-223900-80d9_epoch_30.0/snapshot_iter_19140.caffemodel"
+URL="https://github.com/magnusja/GTSRB-caffe-model/raw/master/caffe/20151207-223900-80d9_epoch_30.0/snapshot_iter_19140.caffemodel"
 BASEDIR=$(dirname "$0")
 cd $BASEDIR
 curl -O $URL
--- model/infer.lua
+++ model/infer.lua
@@ -1,3 +1,16 @@
+require 'image'
 require 'loadcaffe'
+require 'optim'
 
-model = loadcaffe.load('deploy.prototxt', 'snapshot_iter_19140.caffemodel', 'cudnn')
+net = loadcaffe.load('deploy.prototxt', 'snapshot_iter_19140.caffemodel','cudnn')
+net:cuda()
+
+local img = image.load('../static/images/TestSet/00002.ppm')
+
+local input_size = {1,3,227,227}
+local inputs = torch.Tensor(torch.LongStorage(input_size)):cuda()
+local im = image.scale(img,input_size[3],input_size[4])
+inputs[1] = im
+image.display(im)
+outputs = net:forward(inputs)
+print(outputs)
